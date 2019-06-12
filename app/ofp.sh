4798182c5df295b0b165943fabea4b92fa4ea71f
Andrew Halper <ashalper@usgs.gov>
2019-06-12 16:32:38 -0700

    Made paths absolute, and corrected spelling of Output directory.

--- app/ofp.sh
+++ app/ofp.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 python /usr/local/src/onhm-fetcher-parser/pkg/onhm_fp_script.py \
-       -d"$DAYS" \ -i'/var/lib/nhm/ofp/Data' -o'/var/lib/nhm/ofp/Output'
+       -d"$DAYS" -i'/var/lib/nhm/ofp/Data' -o'/var/lib/nhm/ofp/Output'
