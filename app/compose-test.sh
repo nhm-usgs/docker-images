f0e868f049a80b301e542370bbc7be39ecbf495e
Ivan Suftin <isuftin@usgs.gov>
2019-07-11 07:55:31 -0500

    Formatting changes

--- app/compose-test.sh
+++ app/compose-test.sh
@@ -26,21 +26,21 @@ fi
 COMPOSE_FILES="-f docker-compose.yml -f docker-compose-testing.yml"
 echo "Beginning run. If this fails at any point, run the following command:"
 echo "docker-compose $COMPOSE_FILES down"
-for svc in data_loader ofp ncf2cbh nhm-prms out2ncf verifier; do 
+for svc in data_loader ofp ncf2cbh nhm-prms out2ncf verifier; do
     echo ""
     echo "Running ${svc}..."
     docker-compose $COMPOSE_FILES run --rm $svc
-done 
+done
 docker-compose $COMPOSE_FILES down
 
 echo "Pipeline has completed. Will copy output files from Docker Volume"
 echo "Output files will show up in the output directory"
-docker build -t nothing -<<EOF
+docker build -t nothing - <<EOF
 FROM alpine
 CMD
 EOF
 mkdir output
-docker container create --name dummy -v app_nhm:/test  nothing
+docker container create --name dummy -v app_nhm:/test nothing
 docker cp dummy:/test/ofp/Output/ output/
 docker rm dummy
 
