19a8c46f447d685a95228f987f8eca131aa7878f
Andrew Halper <ashalper@usgs.gov>
2020-01-28 13:31:53 -0700

    Initial version.

---
+++ push.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# U.S. Geological Survey
+#
+# File - build.sh
+#
+# Purpose - Publish NHM Docker containers on Docker Hub.
+#
+# Authors -  Andrew Halper
+#
+
+for image in `grep 'image: nhmusgs' docker-compose.yml | cut -d ' ' -f6`; do
+  docker push $image
+done
