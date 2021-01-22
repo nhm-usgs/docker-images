9fdcc046ea033bde0ee7c43bdca054ef9a903817
Andrew Halper <ashalper@usgs.gov>
2021-01-22 10:42:20 -0700

    Factored-out complex docker-compose.yml parsing pipe into services.sh.

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@
 # Authors -  Andrew Halper
 #
 
-# this is only guaranteed to run with yq 3.2.1; yq 4.x query language
-# is different
-for svc in `yq r -p p docker-compose.yml 'services.*' | sed 's/services.//'`; do
+. services.sh			# docker-compose.yml parser funct.
+
+for svc in `services`; do
   docker-compose build "$svc"
 done
